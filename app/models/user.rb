7846c6048de6ccf0e9c53706213b9f3f4c419381
Jameson Trinker <jameson.trinker@gmail.com>
2013-11-05 20:46:18 -0600

    follow/unfollow toggle button

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ActiveRecord::Base
 		foreign_key: :followed_user_id,
 		class_name: 'FollowingRelationship'
 	has_many :followers, through: :follower_relationships 
+
+	def following?(user)
+		followed_user_ids.include?(user.id)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Shouter::Application.routes.draw do
   resource :dashboard, only: [:show]
   
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:index, :new, :create, :show] do
   	post 'follow' => 'following_relationships#create'
-  	# button_to 'Follow', click that and it CREATES a realtionship
+  	# button_to 'Follow', click that and it CREATES a relationship
   	# inside the FollowingRelationshipsController
+    delete 'follow' => 'following_relationships#destroy'
   end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
